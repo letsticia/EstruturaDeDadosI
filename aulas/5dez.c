a36846a55cb2428d3dcc5817c0301a6400530089
Leticia <leti.morais48@gmail.com>
2023-12-05 17:18:16 -0300

    feat: new acknowledgements

--- aulas/5dez.c
+++ aulas/5dez.c
@@ -7,6 +7,7 @@ int main(void){
     // criando um ponteiro que receberá o endereço do primeiro elemento do vetor
     int *vetor = malloc(tamanho*sizeof(int));
 
+
     /*
     para forçar o computador a gerar um edereço de int (ou o tipo que será guardado, utiliza-se um cast)
 
@@ -14,18 +15,14 @@ int main(void){
 
     */
 
+    printf("\nO tamanho atual do vetor é de %d, digite um novo tamanho para ele: ", tamanho);
+    scanf("%d", &novo_tamanho);
+
+    vetor = realloc(vetor, novo_tamanho * sizeof(int));
+
     /*
-    =============== EXEMPLO ======================================  
-
-    vetor -> endereço do primeiro elemento (como se fosse &vetor[0])
-    vetor + 1-> endereço do segundo elemento (como se fosse &vetor[1])
-    vetor + 2-> endereço do terceiro elemento (como se fosse &vetor[2])
-
-    ASSIM, PARA OBTER O CONTEUDO:
-
-    *(vetor) -> conteudo do primeiro elemento
-    *(vetor +1) -> conteudo do segundo elemento
-    *(vetor +2) -> conteudo do terceiro elemento
+        Nas linhas acima, esá havendo uma realocação do tamanho do vetor, de forma a aumentar ou diminuir, 
+        dependendo do input do usuário
     */
 
     if (vetor == NULL){
@@ -42,7 +39,23 @@ int main(void){
         // ou scanf("%d", &vetor[contador]);
     }
 
-    // liberando a memória, uma vez que não será mais utilizado
-    free(vetor);
+        /*
+            sobre o que está ocorrendo na linha 38:
+
+        =================================================================== 
+
+        vetor -> endereço do primeiro elemento (como se fosse &vetor[0])
+        vetor + 1-> endereço do segundo elemento (como se fosse &vetor[1])
+        vetor + 2-> endereço do terceiro elemento (como se fosse &vetor[2])
+
+                        ASSIM, PARA OBTER O CONTEUDO:
+
+        *(vetor) -> conteudo do primeiro elemento
+        *(vetor +1) -> conteudo do segundo elemento
+        *(vetor +2) -> conteudo do terceiro elemento
+    */
+
+   
+    free(vetor); // liberando a memória, uma vez que não será mais utilizado
     return 0;
 }
