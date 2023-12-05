cbaed96199583bcf99a3da879eca1c6a6fe5d16d
Leticia <leti.morais48@gmail.com>
2023-12-05 16:49:12 -0300

    feat: new comments

--- aulas/5dez.c
+++ aulas/5dez.c
@@ -6,11 +6,24 @@ int main(void){
     int *vetor = malloc(3*sizeof(int));
 
     /*
+    para forçar o computador a gerar um edereço de int (ou o tipo que será guardado, utiliza-se um cast)
+
+    int *vetor = (int *) malloc(3*sizeof(int));
+
+    */
+
+    /*
     =============== EXEMPLO ======================================  
 
     vetor -> endereço do primeiro elemento (como se fosse &vetor[0])
-    vetor + 1-> endereço do primeiro elemento (como se fosse &vetor[1])
-    vetor + 2-> endereço do primeiro elemento (como se fosse &vetor[2])
+    vetor + 1-> endereço do segundo elemento (como se fosse &vetor[1])
+    vetor + 2-> endereço do terceiro elemento (como se fosse &vetor[2])
+
+    ASSIM, PARA OBTER O CONTEUDO:
+
+    *(vetor) -> conteudo do primeiro elemento
+    *(vetor +1) -> conteudo do segundo elemento
+    *(vetor +2) -> conteudo do terceiro elemento
     */
 
     if (vetor == NULL){
