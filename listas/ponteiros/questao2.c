2d3046a79604fc203a403ba28cda0b1e37c60062
Letícia <leti.morais48@gmail.com>
2024-01-26 10:35:49 -0300

    feat: added second question c file

---
+++ listas/ponteiros/questao2.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+int main(void) {
+    int x, *p; // declarando x como um inteiro e p como um ponteiro para inteiro
+    x = 100; // inicializando x com 100
+    p = x; // atribuindo o valor de x para p
+    printf("Valor de p = %p\tValor de *p = %d", p, *p); // imprimindo o valor de p e o valor apontado por p
+}
+
+/* Alternativa a) Esta mensagem é de erro ou advertência?
+
+Tendo em vista que, na primeira linha da função main, fora declarado um ponteiro p que receberá 
+o endereço de um valor inteiro (4 bytes) e, entretanto, este não recebera um endereço e sim um valor 
+inteiro normal, provavelmente não será motivo de erro de compilação, já que há espaço o bastante no endereço 
+que este ponteiro está para receber um valor inteiro, assim, será um motivo de advertência.
+
+Apesar disso, ainda é possível que haja uma mensagem de erro na última linha, uma vez que, na função printf, 
+o formatador está esperando dois ponteiros, o primeiro que deveria mostrar o endereço para qual aponta e o segundo que 
+deveria mostrar o o conteúdo deste endereço. Entretanto, é possível que aconteça um erro exatamente nesta linha, uma vez 
+que o programa espera um endereço de memória (que exista) assim como o conteudo deste. Dessa forma, ao tentar acessar o conteúdo 
+de um endereço de memória inexistente, irá gerar um erro (não emitido, mas encerrando o programa sem a execução correta).
+
+*/
+
+/*  Alternativa b) Por que o compilador emite tal mensagem?
+
+Como explicado na alternativa anterior, o compilador inicialmente irá emitir uma advertência 
+porque fora atribuído o valor da varíável x (o valor inteiro, não o endereço) ao ponteiro p. 
+
+E, assim como mencionado também no primeiro item, o erro da  última linha da função main acontece 
+porque o compilador espera um ponteiro para um endereço assim como o conteúdo deste, entretanto, 
+como fora atribuido um valor inteiro, o endereço e o conteúdo não existem. Fazendo com que a função
+printf não seja executada corretamente e, consequentemente, o programa seja encerrado.
+
+*/
+
+/* Alternativa c)  Compile e execute o programa. A execução foi bem sucedida?
+
+```
+listas\ponteiros\questao2.c: In function 'main':
+listas\ponteiros\questao2.c:6:7: warning: assignment makes pointer from integer without a cast [-Wint-conversion]
+     p = x; // atribuindo o valor de x para p
+       ^      
+```
+
+Aparece a advertência mencionada e o comando printf não é exibindo, após isso, o programa se encerra. Dessa forma, 
+a execução não foi bem sucedida tendo em vista que o programa não executa o printf.
+*/
+
+/* Alternativa d) Modifique o trecho de código acima, de modo que nenhuma mensagem seja emitida 
+pelo compilador
+
+Para que nenuma mensage seja exibida do compilador, é necessário fazer a seguinte modificação:
+
+```
+#include <stdio.h>
+
+int main(void) {
+    int x, *p;
+    x = 100;
+    p = &x; #fora adicionado o operador de endereço
+    printf("Valor de p = %p \t Valor de *p = %d", p, *p);
+
+    return 0; # não necessário, mas, por boas práticas, é bom adicionar o return para que saia da função main.
+}
+```
+*/
+
+/* Alternativa e) e) Compile e execute novamente o programa. A execução foi bem sucedida?
+
+Após a compilação e a execução do programa modificado, fora exibido o resultado esperado e nenhum tipo de advertência ou erro.
+
+Resultado da execução:
+
+```
+Valor de p = 0x7ffc63069e3c	Valor de *p = 100
+```
+*/
