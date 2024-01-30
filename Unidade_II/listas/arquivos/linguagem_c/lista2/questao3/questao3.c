9c1b3aaad5f2695346f24a178ed45bb760a80033
Letícia <leti.morais48@gmail.com>
2024-01-30 16:51:16 -0300

    feat: added second exercise

---
+++ Unidade_II/listas/arquivos/linguagem_c/lista2/questao3/questao3.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct fruta{
+    char nome[15];
+    float preco;
+} Fruta;
+
+void preenche_arquivo(){
+    FILE * entrada = fopen("frutas.txt", "a");
+    
+    if(entrada == NULL){
+        printf("Erro ao abrir o arquivo.\n");
+        exit(1);
+    }
+
+    Fruta fruta;
+
+    printf("Digite o nome da fruta: ");
+    scanf(" %[^\n]", fruta.nome);
+    printf("Digite o preco da fruta: ");
+    scanf("%f", &fruta.preco);
+
+    fprintf(entrada, "%s,\t%.2f\n", fruta.nome, fruta.preco);
+
+    fclose(entrada);
+}
+
+int main(){
+    int quantidade;
+
+    printf("Digite a quantidade de frutas que deseja adicionar: ");
+    scanf("%d", &quantidade);
+
+    for (int i = 0; i < quantidade; i++){
+        preenche_arquivo();
+    }
+    return 0;
+}
