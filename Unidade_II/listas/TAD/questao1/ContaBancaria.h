822806dcc7f2645b8026999323e099a53f48997b
Letícia <leti.morais48@gmail.com>
2024-02-07 13:34:17 -0300

    docs: added documentation

--- Unidade_II/listas/TAD/questao1/ContaBancaria.h
+++ Unidade_II/listas/TAD/questao1/ContaBancaria.h
@@ -1,14 +1,20 @@
 /*Definição de um novo tipo: Contabancaria*/
 typedef struct contaBancaria ContaBancaria;
 
+/*Função que aloca memória para um struct contaBancaria, recebe os dados via teclado e retorna um ponteiro de ContaBancaria*/
 ContaBancaria* criaConta(void);
 
+/*Função que deposita um valor na conta, recebe um ponteiro de ContaBancaria e um valor como parâmetro*/
 void deposita(ContaBancaria* conta, float valor);
 
+/*Função que saca um valor da conta, recebe um ponteiro de ContaBancaria e um valor como parâmetro*/
 void saca(ContaBancaria* conta, float valor);
 
+/*Função que transfere um valor de uma conta para outra, recebe dois ponteiros de ContaBancaria e um valor como parâmetro*/ 
 void transfere(ContaBancaria* origem, ContaBancaria* destino, float valor);
 
+/*Função que imprime o saldo da conta, recebe um ponteiro de ContaBancaria como parâmetro*/
 void saldo(ContaBancaria* conta);
 
+/*Função que libera a memória, recebe um ponteiro de ContaBancaria como parâmetro*/
 void excluiConta(ContaBancaria* conta);
