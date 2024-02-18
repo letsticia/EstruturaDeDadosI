1ce7f198842493354d943f34da566dc28bb0622b
Letícia <leti.morais48@gmail.com>
2024-02-18 13:38:36 -0300

    refactor: moved int main to main.c file

--- Unidade_II/listas/TAD_2/questao2/funcionario.c
+++ Unidade_II/listas/TAD_2/questao2/funcionario.c
@@ -33,24 +33,3 @@ void imprime_folha_pagamento(int n, Funcionario** pessoal, char depto){
 
 
 }
-
-int main(){
-    FILE * arquivo = fopen("funcionarios.txt", "r");
-    int n;
-    fscanf(arquivo, "%d\n", &n);
-    Funcionario ** pessoal = (Funcionario **) malloc(n * sizeof(Funcionario *));
-    
-    for(int i = 0; i < n; i++){
-        pessoal[i] = (Funcionario *) malloc(sizeof(Funcionario));
-    }
-
-    copia_dados(arquivo, n, pessoal);
-    
-    imprime_folha_pagamento(n, pessoal, 'A');
-    imprime_folha_pagamento(n, pessoal, 'B');
-    imprime_folha_pagamento(n, pessoal, 'C');
-
-    fclose(arquivo);
-
-    return 0;
-}
