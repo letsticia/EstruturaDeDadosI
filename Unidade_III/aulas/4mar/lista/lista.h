67a2df8b906d980cf7c5b1528fe9cfda409d62fb
letsticia <leti.morais48@gmail.com>
2024-03-05 16:26:42 -0300

    docs: added comments

--- Unidade_III/aulas/4mar/lista/lista.h
+++ Unidade_III/aulas/4mar/lista/lista.h
@@ -12,6 +12,31 @@ Retorno:
     Lista com o valor inserido*/
 Lista* insere_elemento(Lista* lista, int valor);
 
+/* Função que imprime os elementos da lista
+Argumentos: 
+    Lista.
+*/
+void imprime_lista(Lista* lista);
+
+
+/* Função que verifica se a lista está vazia
+Argumentos:
+    Lista
+Retorno:
+    1 - Lista vazia
+    0 - Lista não vazia*/
+int verifica_lista_vazia(Lista* lista);
+
+/*Função que busca um valor na lista
+Argumentos:
+    Lista e valor a ser buscado
+Retorno:
+    1 - Valor encontrado
+    0 - Valor não encontrado
+*/
+int busca_lista(Lista* lista, int valor);
+
+
 /* Função que imprime a lista
 Argumentos: 
     Lista e valor a ser retirado
